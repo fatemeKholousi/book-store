824c5c9b6326e79a8a3ed24887946c00aa851b15
fateme kholousi <Fatemekholousizaer@gmail>
2021-08-15 18:00:13 +0430

    routing is seprated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-
-import Home from "./Components/Home";
-import AdminLogin from "./Components/Login";
-import HomeAdminPanel from "./adminpanel/HomeAdminPanel";
-import NotFound from "./Components/NotFound";
+import Routing from "./Routing";
+import Footer from "./layout/footer";
 import MenuAppBar from "./Components/navbar/MenuAppBar";
 import CustomTheme from "./assets/customRTl";
-import ProtectedRoute from "./Components/ProtectedRoute";
-import Cart from "./Components/cart/Cart";
-import Footer from "./layout/footer";
-import ProductDtails from "./Components/clientpanel/ProductDetails";
-import ShowProductsDependsOnCategory from "./Components/clientpanel/ShowProductsDependsOnCategory";
-import CustomerForm from "./Components/cart/form/CustomerForm";
-import FakePayment from "./Components/cart/payment/FakePayment";
-import PaymentResult from "./Components/cart/payment/PaymentResult";
-
 import {
   ThemeProvider,
   StylesProvider,
@@ -25,6 +12,7 @@ import { create } from "jss";
 import rtl from "jss-rtl";
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
 function App() {
   return (
     <>
@@ -32,32 +20,7 @@ function App() {
         <ThemeProvider theme={CustomTheme}>
           <StylesProvider jss={jss}>
             <MenuAppBar />
-            <Switch>
-              <Route exact path="/login" component={AdminLogin} />
-              <Route
-                exact
-                path="/categories/:category"
-                component={ShowProductsDependsOnCategory}
-              />
-              <Route exact path="/products/:title" component={ProductDtails} />
-              <Route exact path="/cart" component={Cart} />
-              <Route
-                exact
-                path="/customerinfromationform"
-                component={CustomerForm}
-              />
-              <Route exact path="/paymentpage" component={FakePayment} />
-              <Route exact path="/paymentresult" component={PaymentResult} />
-              /**Protected Login Router */
-              <ProtectedRoute
-                exact
-                path="/adminpanel"
-                component={HomeAdminPanel}
-              />
-              {/* <Route exact path="/notfound" component={NotFound} /> */}
-              <Route exact path="/" component={Home} />
-              <Route path="*" component={NotFound} />
-            </Switch>
+            <Routing />
           </StylesProvider>
         </ThemeProvider>
       </div>
---
+++ src/Routing.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Home from "./Components/Home";
+import AdminLogin from "./Components/Login";
+import HomeAdminPanel from "./adminpanel/HomeAdminPanel";
+import NotFound from "./Components/NotFound";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import Cart from "./Components/cart/Cart";
+import ProductDtails from "./Components/clientpanel/ProductDetails";
+import ShowProductsDependsOnCategory from "./Components/clientpanel/ShowProductsDependsOnCategory";
+import CustomerForm from "./Components/cart/form/CustomerForm";
+import FakePayment from "./Components/cart/payment/FakePayment";
+import PaymentResult from "./Components/cart/payment/PaymentResult";
+import { Route, Switch } from "react-router-dom";
+
+function Routing() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/login" component={AdminLogin} />
+        <Route
+          exact
+          path="/categories/:category"
+          component={ShowProductsDependsOnCategory}
+        />
+        <Route exact path="/products/:title" component={ProductDtails} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/customerinfromationform" component={CustomerForm} />
+        <Route exact path="/paymentpage" component={FakePayment} />
+        <Route exact path="/paymentresult" component={PaymentResult} />
+        /**Protected Login Router */
+        <ProtectedRoute exact path="/adminpanel" component={HomeAdminPanel} />
+        {/* <Route exact path="/notfound" component={NotFound} /> */}
+        <Route exact path="/" component={Home} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </>
+  );
+}
+
+export default Routing;
